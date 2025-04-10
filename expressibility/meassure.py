add conjugate circuit to KL calculation and minor fixes

expressibility/meassure.py
@@ -10,7 +10,15 @@ from qiskit.providers.basic_provider import BasicSimulator
 import matplotlib.pyplot as plt
 
 def get_circuit_with_param_conjugate(circuit: QuantumCircuit) -> QuantumCircuit:
-    
+    '''
+    Creates a conjugated version of a parameterized quantum circuit with:
+    - Operation order reversed
+    - Parameters renamed with 'conj_' prefix
+    - Original measurements preserved at the end
+
+    Used for constructing conjugate circuits in KL divergence calculation.
+    '''
+
     qc = circuit.copy()
 
     # Pra manter as mesmas medições, mas no final
@@ -43,46 +51,45 @@ IMAGINARY_UNIT = (-1)**(1/2) # sqrt(-1)
 def P_harr(l,u,N):
     return (1-l)**(N-1)-(1-u)**(N-1)
 
-def get_KL_divergence(circuit : QuantumCircuit, n_shots=10000, nparams=2000, n_bins=75, backend=None):
+def get_KL_divergence(circuit : QuantumCircuit, n_shots = 10000, nparams=2000, reuse_circuit_measures = False, n_bins=75, backend=None) -> float:
+    '''
+    Computes the Kullback-Leibler (KL) divergence between the parameterized
+    quantum circuit and the Haar-random distribution.\n
+    Measures how well the circuit explores the Hilbert space when parameters are randomized.\n
+    Lower values indicate closer resemblance to Haar-random behavior.\n\n
+    + Parameters:\n
+    \t- nparams: Number of random parameter sets to sample
+    \t- n_shots: Measurement shots per parameter set
+    \t- reuse_circuit_measures: Keep existing measurements if True, else measure all qubits
+    \t- n_bins: Histogram bins for probability distribution 
+    '''
     if backend is None: backend = BasicSimulator()
     n_qubits = len(circuit.qubits)
-    b_list = [ i/n_bins for i in range(n_bins+1) ]
-    # b_cent = [b_list[i] + (1/n_bins) for i in range(n_bins)]
+    b_list = [ i/n_bins for i in range(n_bins+1) ] # b_cent = [b_list[i] + (1/n_bins) for i in range(n_bins)]
     harr_hist = [ P_harr(b_list[i], b_list[i+1], 2**n_qubits) for i in range(n_bins) ]
-    zeros = ''; 
-    for _ in range(n_qubits): zeros += '0'
-    # print(circuit)
+    
     conjugado = get_circuit_with_param_conjugate(circuit)
-    # print(conjugado)
+    
+    if not reuse_circuit_measures:
+        conjugado.remove_final_measurements()
+        conjugado.measure_all()
+
+    n_classic = conjugado.num_clbits
+    zeros = ''; 
+    for _ in range(n_classic): zeros += '0'
 
     fidelity=[]    
     for _ in range(nparams):
-
         qc = conjugado.copy()
         qc.assign_parameters({p : 2.0*PI_VALUE*random() for p in qc.parameters}, inplace=True)
         
-        ## precisa do circuito conjugado ?
-
-        qc.remove_final_measurements()
-        qc.measure_all()
-        ## mudar measure all como opcional
-
         counts = backend.run(qc, shots=n_shots).result().get_counts()
-        # print(counts)
 
         ratio = counts.get(zeros, 0) / n_shots
-        # print(ratio)
         fidelity.append(ratio)
-
 
     weights = np.ones_like(fidelity)/float(len(fidelity))
     P_hist=np.histogram(fidelity, bins=b_list, weights=weights, range=[0, 1])[0]
     kl_pq = rel_entr(P_hist, harr_hist)
     
     return sum(kl_pq)
-
-    # plt.hist(fidelity, bins=b_list)
-    # plt.savefig('sla.png')
-    # print('KL(P || Q): %.3f nats' % sum(kl_pq))
-    # print(P_I_hist)
-    # print(kl_pq)