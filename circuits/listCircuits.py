dbd764bb82fef4931ca8d79f8e8bbf13d8b7f2cd
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-06-04 17:43:48 -0300

    add CostaSH circuit images

--- circuits/listCircuits.py
+++ circuits/listCircuits.py
@@ -13,3 +13,23 @@ def get_files_of_type(root_dir, extension) -> str:
 
 def get_circuit_path_list(path='.') -> str:
     return get_files_of_type(path, '.descr')
+
+
+from qiskit import QuantumCircuit
+import matplotlib.pyplot as plt
+
+def save_circuit_image(circuit: QuantumCircuit, file_path: str, dpi: int = 300, style: str = None, ):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    
+    fig = circuit.draw(output='mpl', style=style, fold=50)
+    fig.savefig(file_path, dpi=dpi, bbox_inches='tight', pad_inches=0.1)
+    
+    plt.close(fig)
+
+
+# from circuitFromFile import get_unbound_circuit_from_file
+# for path in get_circuit_path_list('CostaSH'):
+#     name = path.split('/')[-1].split('.')[0]
+#     folder = path.split('/')[-2]
+#     qc = get_unbound_circuit_from_file(path, {'&L' : 1})
+#     save_circuit_image(qc, f'CostaSH/images/{folder}/{name}.jpg')
